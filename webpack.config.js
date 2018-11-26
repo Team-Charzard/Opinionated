bffc6c91c69a2f0e8fb9e37aa440c8d4c7abf849
Erik Fisher <efishr4@gmail.com>
2018-11-26 11:57:51 -0800

    more setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+// Declare HTML Webpack Plugin for Inclusion in WebPack
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  fileName: './index.html',
+});
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+};
